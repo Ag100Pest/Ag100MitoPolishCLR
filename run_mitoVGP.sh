69c6fcf4f9d19b63e35fc1b924a29f6125e0cec0
Astahlke <amanda.stahlke@ceres.scinet.local>
2021-07-20 13:13:41 -0500

    testing canu/2.1.1

--- run_mitoVGP.sh
+++ run_mitoVGP.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
+if [ -z $1 ]; then
+
+        printf "Usage: '$0 <SPECIES> <JOBID>'
+        Runs the modificed mitoVGP pipeline to assemble mitochondrial genomes from pacbio CRL and illumina polishing data"
+        exit 0
+
+elif [ $1 == "-h" ]; then
+
+        cat << EOF
+
+        Usage: '$0 <SPECIES> <JOBID>'
+        Runs the modificed mitoVGP pipeline to assemble mitochondrial genomes from pacbio CRL and illumina polishing data
+EOF
+
+exit 0
+
+fi
 
 mitoVGP=/project/ag100pest/software/modified_mitoVGP
 
 module load canu/1.8 #default is 2.1.1
 
 $mitoVGP/mitoVGP -a pacbio \
-	-s Pectinophora_gossypiella -i Pgos -r NC_041123.1.fasta \
+	-s $SPECIES -i $J -r NC_041123.1.fasta \
 	-t 30 \
 	-1 PB_list.txt \
 	-2 I_list.txt \
