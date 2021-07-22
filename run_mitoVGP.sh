716ce59edd3b1285ecf6c0c95ea5384625a33f4b
Astahlke <amanda.stahlke@ceres.scinet.local>
2021-07-22 16:25:18 -0500

    re-fromated slurm submission strategy

--- run_mitoVGP.sh
+++ run_mitoVGP.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#SBATCH -n 30
+#SBATCH -p short
+#SBATCH --job-name=CLR_mitoVGP         #<= name your job
+#SBATCH --output=R-%x.%J.out          # standard out goes to R-JOBNAME.12345.out
+#SBATCH --error=R-%x.%J.err           # standard error goes to R-JOBBNAME.12345.err
+#SBATCH --mail-user=amandastahlke@gmail.com    #<= Your email
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+
 if [ -z $1 ]; then
 
         printf "Usage: '$0 <SPECIES> <JOBID> <REF_MT> <PACBIO_LIST> <I_LIST>'
@@ -17,7 +26,11 @@ exit 0
 
 fi
 
+module load miniconda
+
 mitoVGP=/project/ag100pest/software/modified_mitoVGP
+source activate $mitoVGP/pacbio_mitoVGP
+
 SPECIES=$1
 J=$2
 REF=$3
@@ -29,14 +42,18 @@ printf "Job id is  $J \n"
 printf "Reference mt genome is $REF \n"
 printf "Input pacbio reads are $PB_LS \n"
 printf "Input pacbio reads are $I_LS \n"
+printf "Output is $OUT \n"
+
+OUT=/project/ag100pest/$SPECIES/RawData/MT_Contig/
+mkdir -p $OUT/mitoVGP
 
 ## pipeline is sensitive to canu version. throw error if not 2.1
 module load canu/2.1 #default is 2.1.1
 
 $mitoVGP/mitoVGP -a pacbio \
-	-s $SPECIES -i $J -r NC_041123.1.fasta \
+	-s $SPECIES -i $J -r $REF \
 	-t 30 \
-	-1 PB_list.txt \
-	-2 I_list.txt \
+	-1 $PB_LS \
+	-2 $I_LS \
 	-z 5000 # increases sesnitivity of mummer
 
--- submit_mtAsmPolishCLR.sh
+++ submit_mtAsmPolishCLR.sh
@@ -1,25 +1,58 @@
 #! /usr/bin/env bash
-#SBATCH -n 30
-#SBATCH -p short
-#SBATCH --job-name=CLR_mitoVGP         #<= name your job
-#SBATCH --output=R-%x.%J.out          # standard out goes to R-JOBNAME.12345.out
-#SBATCH --error=R-%x.%J.err           # standard error goes to R-JOBBNAME.12345.err
-#SBATCH --mail-user=amandastahlke@gmail.com    #<= Your email
-#SBATCH --mail-type=begin
-#SBATCH --mail-type=end
+
+if [ -z $1 ]; then
+
+        printf "Usage: '$0 <SPECIES> <JOBID> <REF_MT> <PACBIO_LIST> <I_LIST>'
+        Submits the modified mitoVGP pipeline to assemble mitochondrial genomes from pacbio CRL and illumina polishing data"
+        exit 0
+
+elif [ $1 == "-h" ]; then
+
+        cat << EOF
+
+        Usage: '$0 <SPECIES> <JOBID> <REF_MT> <PACBIO_LIST> <I_LIST>'
+        Runs the modified mitoVGP pipeline to assemble mitochondrial genomes from pacbio CRL and illumina polishing data
+EOF
+
+exit 0
+
+fi
+
 
 #=== Load any modules
-export PATH=$PATH:/project/ag100pest/software/modified_mitoVGP/
+#export PATH=$PATH:/project/ag100pest/software/modified_mitoVGP/
 mitoVGP=/project/ag100pest/software/modified_mitoVGP
+#source activate $mitoVGP/pacbio_mitoVGP
 
 #=== Main program
-# printf "running on species $1 with jobid $2"
-SPECIES=$1 #"Pectinophora_gossypiella" # hardcoded for now
-I=$2 #"Pgos5"
+SPECIES=$1
+J=$2
+REF=$3
+PB_LS=$4
+I_LS=$5
+OUT=/project/ag100pest/$SPECIES/RawData/MT_Contig
 
-mkdir -p /project/ag100pest/Pgos/RawData/MT_Contig/mitoVGP
-cd /project/ag100pest/Pgos/RawData/MT_Contig/mitoVGP
-source activate $mitoVGP/pacbio_mitoVGP
-$mitoVGP/run_mitoVGP.sh $SPECIES $I  # hardcoded for Pgos
+printf "Species is $SPECIES \n"
+printf "Job id is  $J \n"
+printf "Reference mt genome is $REF \n"
+printf "Input pacbio reads are $PB_LS \n"
+printf "Input pacbio reads are $I_LS \n"
+printf "Output is $OUT \n"
+
+mkdir -p $OUT/mitoVGP
+cd $OUT
+sbatch $mitoVGP/run_mitoVGP.sh $SPECIES $J $REF $PB_LS $I_LS
+
+#==== Eval with merqury
+module load merqury/1.1
+module load meryl/1.0
+
+MERQ=/project/ag100pest/software/merqury
+
+printf "out meryl db in ${J}.meryl" # supplied by toplevel mitovgp script
+
+mkdir -p $OUT/qv
+cd $OUT
+sbatch $MERQ/_submit_build.sh 31 $I_LS $J
 
 
